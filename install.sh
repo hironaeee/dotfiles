67e800a6436d58b53deb4a44b37f05c970b1807e
hironaeee <c3.sazae3@gmail.com>
2021-05-03 10:31:11 +0900

    Add .hushlogin

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 cd "$(dirname "{BASH_SOURCE[0]}")" || exit 1
 
 BASE=$(pwd -P)
-for rc in .*rc .*profile .*.conf .*config; do
+for rc in .*rc .*profile .*.conf .*config .hushlogin; do
   mkdir -pv bak
   [ -e ~/"$rc" ] && mv -v ~/"$rc" bak/"$rc"_$(date '+%Y%m%d%H%M%S')
   ln -sfv "$BASE/$rc" ~/"$rc"
