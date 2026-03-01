416ab7238fe27d3c6dcc73f4dc3b9da6d8b4f719
hironaeee <c3.sazae3@gmail.com>
2026-03-01 22:43:07 +0900

    update: nvim-tresitter plugin getting from 'main' repository

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -3,68 +3,79 @@ return {
 		"neovim/nvim-lspconfig",
 		lazy = false,
 		dependencies = {
-			"williamboman/mason.nvim",
-			"williamboman/mason-lspconfig.nvim",
-		},
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-			-- LSP servers auto installation
-			require("mason").setup({})
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"ts_ls",
-					"tailwindcss",
-					"gopls",
-				},
-				handlers = {
-					function(server_name) -- default handler
-						require("lspconfig")[server_name].setup({
-							capabilities = capabilities,
-						})
-					end,
-				},
-				settings = {
-					experimental = {
-						useFlatConfig = true,
+			{
+				"williamboman/mason.nvim",
+				opts = {
+					registries = {
+						"github:mason-org/mason-registry",
+						"github:Crashdummyy/mason-registry",
 					},
 				},
-			})
+			},
+			{
+				"williamboman/mason-lspconfig.nvim",
+				opts = {
+					ensure_installed = {
+						"lua_ls",
+						"ts_ls",
+						"tailwindcss",
+					},
+					settings = {
+						experimental = {
+							useFlatConfig = true,
+						},
+					},
+				},
+			},
+			{
+				"jay-babu/mason-null-ls.nvim",
+				event = { "BufReadPre", "BufNewFile" },
+				dependencies = {
+					"nvimtools/none-ls.nvim",
+				},
+				opts = {
+					ensure_installed = {
+						"stylua",
+						"prettier",
+						"eslint_d",
+					},
+				},
+			},
+		},
+		config = function()
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+				callback = function(ev)
+					local opts = { buffer = ev.buf, silent = true }
 
-			vim.diagnostic.config({
-				virtual_text = true,
+					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+					vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+					vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+					-- vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+					vim.keymap.set("n", "gr", "<cmd>Telescope lsp_references<Return>", opts)
+					vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
+					vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+					vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+					vim.keymap.set("n", "<leader>dn", function()
+						vim.diagnostic.jump({ count = 1, float = true })
+					end, opts)
+					vim.keymap.set("n", "<leader>dN", function()
+						vim.diagnostic.jump({ count = -1, float = true })
+					end, opts)
+					vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, opts)
+				end,
 			})
-
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
-			vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
-			vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, {})
-			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
-			vim.keymap.set("n", "<leader>dn", vim.diagnostic.goto_next, {})
-			vim.keymap.set("n", "<leader>dN", vim.diagnostic.goto_prev, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-			vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
 		end,
 	},
 	{
-		"jay-babu/mason-null-ls.nvim",
-		event = { "BufReadPre", "BufNewFile" },
-		dependencies = {
-			"williamboman/mason.nvim",
-			"nvimtools/none-ls.nvim",
+		"folke/lazydev.nvim",
+		ft = "lua",
+		opts = {
+			library = {
+				-- Load luvit types when the `vim.uv` word is found
+				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+			},
 		},
-		config = function()
-			-- Linter / Formatter auto installation
-			require("mason-null-ls").setup({
-				ensure_installed = {
-					"stylua",
-					"prettier",
-					"eslint_d",
-				},
-			})
-		end,
 	},
 }
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -2,26 +2,30 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	config = function()
-		local config = require("nvim-treesitter.configs")
-		config.setup({
-			highlight = {
-				enable = true,
-				disable = {},
-			},
-			indent = {
-				enable = true,
-				disable = {},
-			},
-			ensure_installed = {
+		local treesitter_path = vim.fs.joinpath(vim.fn.stdpath("data"), "/treesitter")
+		vim.uv.fs_mkdir(treesitter_path, tonumber("755", 8))
+
+		local nvim_treesitter = require("nvim-treesitter")
+
+		nvim_treesitter.setup({
+			install_dir = treesitter_path,
+		})
+		nvim_treesitter
+			.install({
 				"tsx",
 				"lua",
 				"json",
 				"css",
 				"go",
-			},
-			diagnostics = {
-				disable = { "missing-fields" },
-			},
+			})
+			:wait(300000) -- max wait time of 5 minutes
+
+		-- enable highlighting automatically
+		vim.api.nvim_create_autocmd("FileType", {
+			group = vim.api.nvim_create_augroup("vim-treesitter-start", {}),
+			callback = function(_)
+				pcall(vim.treesitter.start)
+			end,
 		})
 	end,
 }
